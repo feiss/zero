4d8cf08b47aa54b750aa83439a212e9149523e5b
feiss <github@feiss.be>
2019-06-03 01:34:47 +0200

    js/canvas: added zero_events()

--- src/js/canvas/zero.js
+++ src/js/canvas/zero.js
@@ -1,6 +1,24 @@
 
 var _z = {};
 
+const RESIZE = 1;
+const KEY_DOWN = 2;
+const KEY_UP = 3;
+const MOUSE_DOWN = 4;
+const MOUSE_UP = 5;
+const MOUSE_MOVE = 6;
+const MOUSE_LEAVE = 7;
+const MOUSE_ENTER = 8;
+
+const SHIFT = 1;
+const CONTROL = 2;
+const ALT = 4;
+const SUPER = 8;
+
+const MOUSE_LEFT = 0;
+const MOUSE_RIGHT = 1;
+const MOUSE_MIDDLE = 2;
+
 function zero_open(el, w, h, d, s) {
   var canvas = document.createElement('canvas');
   var dirtyCanvas = document.createElement('canvas');
@@ -45,5 +63,42 @@ function zero_update(fb, pal) {
   _z.ctx.drawImage(_z.dirtyCtx.canvas, 0, 0, canvas.width, canvas.height);
 }
 
-
 function zero_close() {}
+
+function zero_events(handler){
+  if (_z.handler) {
+    _z.ctx.canvas.removeEventListener('mousedown', __mouse_handler);
+    _z.ctx.canvas.removeEventListener('mouseup', __mouse_handler);
+    _z.ctx.canvas.removeEventListener('mousemove', __mouse_handler);
+    document.body.removeEventListener('keydown', __key_handler);
+    document.body.removeEventListener('keyup', __key_handler);
+    window.removeEventListener('resize', __resize_handler);
+  }
+
+  _z.handler = handler;
+  _z.ctx.canvas.addEventListener('mousedown', __mouse_handler);
+  _z.ctx.canvas.addEventListener('mouseup', __mouse_handler);
+  _z.ctx.canvas.addEventListener('mousemove', __mouse_handler);
+  document.body.addEventListener('keydown', __key_handler);
+  document.body.addEventListener('keyup', __key_handler);
+  window.addEventListener('resize', __resize_handler);
+}
+
+function __mouse_handler (evt) {
+  const MAP_BUTTON = [0, 2, 1];
+  var type = {'mousedown': MOUSE_DOWN, 'mouseup': MOUSE_UP, 'mousemove': MOUSE_MOVE}[evt.type];
+  if (type === undefined) return;
+  let param1 = type === MOUSE_MOVE ? evt.offsetX : MAP_BUTTON[evt.button];
+  let param2 = type === MOUSE_MOVE ? evt.offsetY : MAP_BUTTON[evt.button];
+  _z.handler(type, param1, param2);
+}
+
+function __key_handler (evt) {
+  const modifiers = evt.metaKey << 3 | evt.altKey << 2 | evt.controlKey << 1 | evt.shiftKey;
+  _z.handler(evt.type === 'keydown' ? KEY_DOWN : KEY_UP, evt.keyCode, modifiers);
+}
+
+function __resize_handler (evt) {
+  _z.handler(RESIZE, window.innerWidth, window.innerHeight);
+}
+
