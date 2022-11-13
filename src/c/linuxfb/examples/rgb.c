b1f807f687aed08c1cdbad14648852ef8baad6ab
Diego F. Goberna <github@feiss.be>
2022-11-13 08:38:16 +0100

    fixed linuxfb

--- src/c/linuxfb/examples/rgb.c
+++ src/c/linuxfb/examples/rgb.c
@@ -5,20 +5,19 @@ void main(void) {
   zero_open(NULL, &w, &h, &d, 1, &line);
 
   char *fb = (char *)malloc(w * h * d);
-  char *p;
+  char *p=fb;
 
-  for(int y = 0; y < 256; y++) {
-    p = fb + y * line;
-    for(int x = 0; x < 256; x++) {
-      p[0] = y;
-      p[1] = x;
-      p[2] = 255 - x;
+  for(int y = 0; y < h; y++) {
+    for(int x = 0; x < w; x++) {
+      p[0] = (int)(((float)x/w)*255.f);
+      p[1] = (int)(((float)y/h)*255.f);
+      p[2] = 0;
       p += d;
     }
   }
   
-  // 5 secs, assuming fbdev being 60fps
-  int loop = 60 * 5;
+  // 3 secs, assuming fbdev being 60fps
+  int loop = 60 * 3;
   while(--loop) {
     zero_update(fb, NULL);
   }
